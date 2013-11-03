57e06b8ac52060d9d5053e67ef9ad1a680393173
Yordan Shterev <shterew@gmail.com>
2013-11-03 19:27:07 +0100

    End of test section

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,12 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # test "product is not valid without a unique title" do
+  #   product = Product.new(:title => products(:ruby).title,
+  #   :description => "yyy",
+  #   :price => 1, :image_url => "fred.gif")
+  #   assert !product.save
+  #   assert_equal "has already been taken", product.errors[:title].join('; ')
+  # end
 end
