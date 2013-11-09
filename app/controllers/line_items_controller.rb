8186e57779ea66e1003c230bbd72a278195f3382
Yordan Shterev <shterew@gmail.com>
2013-11-09 23:27:42 +0100

    Added delete button for each row in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -58,10 +58,9 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1.json
   def destroy
     @line_item.destroy
-    respond_to do |format|
-      format.html { redirect_to line_items_url }
-      format.json { head :no_content }
-    end
+
+    redirect_to(:back)
+
   end
 
   private
